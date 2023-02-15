a3b15c30995b450602186954ca2067e309d5ed4a
ricardodragon <ricardoelfuego@gmail.com>
2023-02-15 15:09:38 -0300

    teste

---
+++ src/main/java/com/dibros/gateway/security/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.dibros.gateway.security.config;
+
+import com.dibros.core.token.property.JwtConfiguration;
+import com.dibros.gateway.security.filter.GatewayJwtTokenAuthorizationFilter;
+import com.dibros.core.token.config.TokenConfig;
+import com.dibros.core.token.converter.TokenConverter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends TokenConfig {
+
+    private final TokenConverter tokenConverter;
+
+    public SecurityConfig(JwtConfiguration jwtConfiguration, TokenConverter tokenConverter) {
+        super(jwtConfiguration);
+        this.tokenConverter = tokenConverter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterAfter(new GatewayJwtTokenAuthorizationFilter(jwtConfiguration, tokenConverter), UsernamePasswordAuthenticationFilter.class);
+        super.configure(http);
+    }
+}
